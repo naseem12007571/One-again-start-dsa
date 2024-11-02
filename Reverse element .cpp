d6205e274a1536afc7ec9eca4b8ed012a436e7c3
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-11-02 22:23:58 +0530

    Update Reverse the Array  element .cpp

--- Reverse element .cpp
+++ Reverse element .cpp
@@ -1,27 +1,29 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
+    
     int n;
-    cout<<"Enter the size of array ";
+    cout<<"ENTER THE size of array n = ";
     cin>>n;
-     
-    cout<<"Enter the array element= ";
+    
     int arr[100];
+    cout<<"Enter the array element ";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     
-    int start=0;
-    int end=n-1;
+    int start =0;
+    int end= n-1;
     
-    while(start<end){
+    while(start<=end){
         swap(arr[start],arr[end]);
-        start++;
-        end--;
+        start++,end--;
     }
-    cout<<"reveres element is = ";
+    
+    
+    cout<<"Reverse the Array element here = ";
     for(int i=0;i<n;i++){
-        cout<<arr[i];
+        cout<<arr[i]<<" ";
     }
 }
