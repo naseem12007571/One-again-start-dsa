ff8458705d735c09dd5216aea85c4bc6ac263bb3
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-11-05 12:46:52 +0530

    Create fibonacci sequnce or series.cpp

---
+++ fibonacci sequnce or series.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int n;
+    cout<<"Enter the number n = ";
+    cin>>n;
+    
+    int first =0 , second = 1, third;
+    cout<< first<< " "<< second;
+    for(int i=2;i<n;i++){
+       third= first+second;
+       cout<<" "<< third;
+       first= second;
+       second= third;
+        
+    }
+    // cout<<"fibonacci number is "<<second;
+}
+
+
+// ager only sequnce find kerna h 8 ka kya hoga
+// chnage here it lop <=n tk chailega or dont print first and second first in last second
